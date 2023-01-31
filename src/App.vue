f357feb064e3b7bb3e609081579e1ef377fc1a5d
YutaroTakei <yutarotakei@icloud.com>
2023-01-31 21:06:11 +0900

    ヘッダーとフッター、サイドメニューの追加

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,87 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    
+      <v-navigation-drawer app v-model="drawer" clipped >
+        <v-container>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="title grey--text text--darken-2">
+                Navigation lists
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
 
+          <v-divider></v-divider>
+
+          <v-list dense nav>
+            <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
+              <v-list-item-icon>
+                <v-icon>{{ nav_list.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-container>
+      </v-navigation-drawer>
+
+
+    <v-app-bar color="primary" dark app clipped-left>
+    <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuetify</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-toolbar-items>
+      <v-btn text>For Enterprise</v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+        <v-btn v-on="on" text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
+        </template>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-list>
+          <v-subheader>Get help</v-subheader>
+            <v-list-item v-for="support in supports" :key="support.name">
+              <v-list-item-icon>
+              <v-icon>{{ support.icon }}</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+              <v-list-item-title>{{ support.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+        </v-list>
+
+
+      </v-menu>
+    </v-toolbar-items>
     </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-footer color="primary" dark app>
+      Vuetify
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  data(){
+    return{
+        drawer: null,
+        supports:[
+          {name: 'Consulting and suppourt',icon: 'mdi-vuetify'},
+          {name: 'Discord community',icon: 'mdi-discord'},
+          {name: 'Report a bug',icon: 'mdi-bug'},
+          {name: 'Github issue board',icon: 'mdi-github'},
+          {name: 'Stack overview',icon: 'mdi-stack-overflow'},
+        ],
+          nav_lists:[
+          {name: 'Getting Started',icon: 'mdi-vuetify'},
+          {name: 'Customization',icon: 'mdi-cogs'},
+          {name: 'Styles & animations',icon: 'mdi-palette'},
+          {name: 'UI Components',icon: 'mdi-view-dashboard'},
+          {name: 'Directives',icon: 'mdi-function'},
+          {name: 'Preminum themes',icon: 'mdi-vuetify'},
+  ]
+    }
+  }
+}
 </script>
