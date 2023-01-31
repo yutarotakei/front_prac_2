a69b3b262d05c437fed1361d750d42f7b1bb7e68
YutaroTakei <yutarotakei@icloud.com>
2023-02-01 00:57:29 +0900

    メインコンテンツ部分の作成とvue-routerの導入

--- src/App.vue
+++ src/App.vue
@@ -14,19 +14,27 @@
 
           <v-divider></v-divider>
 
-          <v-list dense nav>
-            <v-list-item v-for="nav_list in nav_lists" :key="nav_list.name">
-              <v-list-item-icon>
-                <v-icon>{{ nav_list.icon }}</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+          <v-list nav dense>
+            <v-list-group 
+            v-for="nav_list in nav_lists" 
+            :key="nav_list.name" 
+            :prepend-icon="nav_list.icon" 
+            no-action 
+            :append-icon="nav_list.lists ? undefined : ''"> 
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>{{ nav_list.name }}</v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-list-item v-for="list in nav_list.lists" :key="list">
+                <v-list-item-content>
+                  <v-list-item-title>{{ list }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
           </v-list>
         </v-container>
       </v-navigation-drawer>
-
 
     <v-app-bar color="primary" dark app clipped-left>
     <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
@@ -55,9 +63,16 @@
       </v-menu>
     </v-toolbar-items>
     </v-app-bar>
-    <v-footer color="primary" dark app>
+
+<v-main>
+  <router-view />
+</v-main>
+
+
+
+  <v-footer color="primary" dark app>
       Vuetify
-    </v-footer>
+  </v-footer>
   </v-app>
 </template>
 
@@ -73,14 +88,35 @@ export default {
           {name: 'Github issue board',icon: 'mdi-github'},
           {name: 'Stack overview',icon: 'mdi-stack-overflow'},
         ],
-          nav_lists:[
-          {name: 'Getting Started',icon: 'mdi-vuetify'},
-          {name: 'Customization',icon: 'mdi-cogs'},
-          {name: 'Styles & animations',icon: 'mdi-palette'},
-          {name: 'UI Components',icon: 'mdi-view-dashboard'},
-          {name: 'Directives',icon: 'mdi-function'},
-          {name: 'Preminum themes',icon: 'mdi-vuetify'},
-  ]
+        nav_lists:[
+          {
+            name: 'Getting Started',
+            icon: 'mdi-speedometer',
+            lists:['Quick Start','Pre-made layouts']
+          },
+          {
+            name: 'Customization',
+            icon: 'mdi-cogs' 
+          },
+          {
+            name: 'Styles & animations',
+            icon: 'mdi-palette',
+            lists:['Colors','Content','Display']
+          },
+          {
+            name: 'UI Components',
+            icon: 'mdi-view-dashboard',
+            lists:['API explorer','Alerts']
+          },
+          {
+            name: 'Directives',
+            icon: 'mdi-function'
+          },
+          {
+            name: 'Preminum themes',
+            icon: 'mdi-vuetify'
+          },
+        ]
     }
   }
 }
